80342c467f7270d01559e37d8ef04dbc9fdee001
Nick Neiert <nickster7421@gmail.com>
2019-02-11 14:33:39 -0600

    added GET route to dualsport, hiking, photography and road server routes

--- server/routes/hiking.router.js
+++ server/routes/hiking.router.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
  * GET route template
  */
 router.get("/", (req, res) => {
-    if (req.isAuthenticated()) {
-      console.log("req.user:", req.user);
-      pool.query(`SELECT * FROM "user_adventure" JOIN "adventure_name" 
+  if (req.isAuthenticated()) {
+    console.log("req.user:", req.user);
+    pool
+      .query(`SELECT * FROM "user_adventure" JOIN "adventure_name" 
                 ON "user_adventure"."adventure_id" ="adventure_name"."id"  WHERE "advent_type_id" = 1
-                AND ${req.user.id} = "adventure_name"."person_id"`)
-        .then(results => res.send(results.rows))
-        .catch(error => {
-          console.log("error");
-          res.sendStatus(403);
-        });
-    } else {
-      //They are not authenticated.
-      res.sendStatus(403);
-    }
-  });
+                AND ${req.user.id} = "adventure_name"."person_id"`
+      )
+      .then(results => res.send(results.rows))
+      .catch(error => {
+        console.log("error");
+        res.sendStatus(403);
+      });
+  } else {
+    //They are not authenticated.
+    res.sendStatus(403);
+  }
+});
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-
-});
+router.post("/", (req, res) => {});
 
 module.exports = router;
