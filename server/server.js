ba9002b31755a895eca9b264db42305bc918b9ba
Nick Neiert <nickster7421@gmail.com>
2019-02-11 13:19:23 -0600

    added base server side routes

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const hikingRouter= require('./routes/hiking.router');
+const dualsportRouter = require('./routes/dualsport.router');
+const roadRouter = require('./routes/road.router');
+const photographyRouter = require('./routes/photography.router');
+const recentRouter = require('./routes/recent.router');
+
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +30,13 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/hiking',hikingRouter);
+app.use('/api/dualsport',dualsportRouter);
+app.use('/api/road',roadRouter);
+app.use('/api/photography',photographyRouter);
+app.use('/api/recent',recentRouter);
+
+
 
 // Serve static files
 app.use(express.static('build'));
