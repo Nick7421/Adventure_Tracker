4132de79f2d198d9a7052aa47bc570977350aaab
Nick Neiert <nickster7421@gmail.com>
2019-02-12 13:46:53 -0600

    added base Recent adventure component for the starting page.

---
+++ src/components/RecentAdventure/RecentAdventure.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class RecentAdventure extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Recent Adventures</h1>
+      </div>
+    )
+  }
+}
+export default RecentAdventure;
