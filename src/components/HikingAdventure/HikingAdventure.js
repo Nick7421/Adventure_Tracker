aecfa67396410b908ecef230ddaa801936a8497e
Nick Neiert <nickster7421@gmail.com>
2019-02-18 09:30:45 -0600

    added reduxStore to hikingAdventure.js. JSON shows on the page.

--- src/components/HikingAdventure/HikingAdventure.js
+++ src/components/HikingAdventure/HikingAdventure.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
 
 class HikingAdventure extends Component {
+  componentDidMount(){
+    this.getHiking();
+  }
+
+  getHiking = () => {
+    const action = {type:'GET_HIKING'};
+    this.props.dispatch(action);
+  }
+
   render() {
     return (
       <div>
         <h1>Hiking Adventures</h1>
+        {JSON.stringify(this.props.reduxStore.hikingReducer)}
       </div>
     )
   }
 }
-export default HikingAdventure
+const mapStoreToProps = reduxStore => ({
+  reduxStore
+});
+
+export default connect(mapStoreToProps)(HikingAdventure);
