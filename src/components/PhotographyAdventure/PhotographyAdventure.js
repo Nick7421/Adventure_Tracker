8ac63f4fc4c5e41b6a64a6ca2857dfce6e3b09dc
Nick Neiert <nickster7421@gmail.com>
2019-02-18 10:42:49 -0600

    modified photographyAdventure component to connect to the redux store

--- src/components/PhotographyAdventure/PhotographyAdventure.js
+++ src/components/PhotographyAdventure/PhotographyAdventure.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
 
 class PhotographyAdventure extends Component {
+  componentDidMount(){
+    this.getPhotography();
+  }
+
+  getPhotography = () => {
+    const action = {type:'GET_PHOTOGRAPHY'};
+    this.props.dispatch(action);
+  }
   render() {
     return (
       <div>
         <h1>Photography Adventures</h1>
+        {JSON.stringify(this.props.reduxStore.photographyReducer)}
       </div>
     )
   }
 }
-export default PhotographyAdventure 
+const mapStoreToProps = reduxStore => ({
+  reduxStore
+});
+
+export default connect(mapStoreToProps)(PhotographyAdventure);
