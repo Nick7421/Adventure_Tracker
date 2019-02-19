e9c93a837a50c4ef5f7218fd6b657f553cc0dc13
Nick Neiert <nickster7421@gmail.com>
2019-02-19 14:40:49 -0600

    added state and GET for details page component

--- server/routes/detail.router.js
+++ server/routes/detail.router.js
@@ -7,14 +7,15 @@ const router = express.Router();
  */
 router.get('/:id', (req, res) => {
     if (req.isAuthenticated()) {
-      console.log(req.params);
+      console.log(req.params.id);
         // console.log("req.user:", req.user);
         //SELECT * FROM "user_adventure" WHERE "adventure_id" = 16;
         pool.query(`SELECT * FROM "user_adventure" WHERE "adventure_id" =
                     ${req.params.id};`)
-          .then(results => res.send(results.rows))
-          .catch(error => {
-            console.log("error");
+          .then((result) => {
+            res.send(result.rows)
+          }).catch(error => {
+            console.log("error",error);
             res.sendStatus(403);
           });
       } else {
--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import GoogleMaps from '../GoogleMaps/GoogleMaps';
+import DetailsItem from './DetailsItem';
 
 class DetailsPage extends Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+          data: []
+        };
     }
-    compnentDidMount(){
+    componentDidMount(){
         this.getDetails();
     }
 
-    getDetails = (action) => {
-    const Id = action.payload;
+    getDetails = () => {
+    const id = this.props.match.params.id;
+    console.log('int get details');
     axios({
         method:'GET',
-        url:`/api/detail/${Id}`
-    }).then((response)=>{
+        url: `/api/detail/${id}`
+    }).then((response) => {
       console.log(response.data);
-        this.setState(response.data);
+        this.setState({
+          test:response.data,
+        })
     })
 
     }
@@ -27,6 +33,8 @@ class DetailsPage extends Component {
       <div>
       {JSON.stringify(this.props.match.params.id)}
       {JSON.stringify(this.state)}
+      
+
         <h1>Details Page</h1>
         
         <GoogleMaps />
